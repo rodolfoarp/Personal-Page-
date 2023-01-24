013372ccb3942b1138ac0806714cae2e64678d7b
Rodolfo Ramirez <ramirezrodolfo333@gmail.com>
2023-01-23 23:52:44 -0400

    Update React Router

--- src/header.js
+++ src/header.js
@@ -1,32 +1,45 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 class App extends React.Component {
-    render() {
-        return (
-            <div className='Header'>
-                <header>
-                    <div className="header-list">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                        </svg>
-                    </div>
-
-                    <nav className="header-nav">
-                        <ul className="header-nav__list">
-                            <li className="header-nav__item header-nav__description"><a href="">Rodolfo Ramirez</a></li>
-                            <li className="header-nav__item header-nav__home"><a href="index.html" rel="noreferrer noopener">Home</a></li>
-                            <li className="header-nav__item header-nav__about"><a href="">About</a></li>
-                        </ul>
-                    </nav>
-
-                    <div className="header-mode">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="currentColor" class="bi bi-sun" viewBox="0 0 16 16">
-                            <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-                        </svg>
-                    </div>
-                </header>
-                <hr></hr>
-            </div>
-        )
+    render(){
+        return(
+            <Router>
+                <div className='Header'>
+                    <header>
+                        <Navbar>
+                            <Switch>
+                                <Route path='/' component={Home}></Route>
+                                <Route path='/home' component={Home}></Route>
+                                <Route path='/about' component={About}></Route>
+                            </Switch>
+                        </Navbar>
+                    </header>
+                </div>
+            </Router>
+        );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('initial-app'))
+
+class Navbar extends React.Component {
+    render(){
+        return(
+            <nav className="header-nav">
+                <ul className="header-nav__list">
+                    <li className="header-nav__item header-nav__description">
+                        <Link to="/">Rodolfo Ramirez</Link>
+                    </li>
+                    <li className="header-nav__item header-nav__description">
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li className="header-nav__item header-nav__description">
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('initial-app'));
+root.render(<App />);
